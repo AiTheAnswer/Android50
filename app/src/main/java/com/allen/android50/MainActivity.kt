b6f257a61177ba93b2750fdc0f265826ae24feb4
Allen <1576758110@qq.com>
2018-07-27 09:17:27 +0800

    添加Notification的实现

--- app/src/main/java/com/allen/android50/MainActivity.kt
+++ app/src/main/java/com/allen/android50/MainActivity.kt
@@ -3,6 +3,9 @@ package com.allen.android50
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.Toolbar
 import android.view.View
 import android.widget.Button
 import com.allen.cardview.CardViewActivity
@@ -13,12 +16,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private var mBtnRecyclerView: Button? = null
     private var mBtnCardView: Button? = null
     private var mBtnNotification: Button? = null
+    private var mToolbar: Toolbar? = null
+    private var mDrawerLayout: DrawerLayout? = null
+    private var mDrawerToggle: ActionBarDrawerToggle? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initView()
         initListener()
+        setSupportActionBar(mToolbar)
+        mDrawerToggle = ActionBarDrawerToggle(this,mDrawerLayout,mToolbar,R.string.drawer_open,R.string.drawer_close)
+        mDrawerToggle?.syncState()
+        mDrawerLayout?.addDrawerListener(mDrawerToggle!!)
     }
 
     override fun onClick(view: View?) {
@@ -51,5 +61,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         mBtnRecyclerView = findViewById(R.id.recycler_view)
         mBtnCardView = findViewById(R.id.card_view)
         mBtnNotification = findViewById(R.id.notification)
+        mToolbar = findViewById(R.id.toolbar)
+        mDrawerLayout = findViewById(R.id.drawer_layout)
     }
 }
